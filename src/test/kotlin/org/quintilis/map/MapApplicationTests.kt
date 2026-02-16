eecd47939e5fd75d7e3bb1826440c99d5409a486
rafinha285 <rafael.schrott02@gmail.com>
2026-02-16 02:47:54 -0300

    Remove Tests

--- src/test/kotlin/org/quintilis/map/MapApplicationTests.kt
+++ src/test/kotlin/org/quintilis/map/MapApplicationTests.kt
@@ -3,10 +3,10 @@ package org.quintilis.map
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+//@SpringBootTest
 class MapApplicationTests {
 
-    @Test
+//    @Test
     fun contextLoads() {
     }
 
